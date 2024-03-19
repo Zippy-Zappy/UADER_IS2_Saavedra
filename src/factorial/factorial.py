9c246e7b915315829d1ee23845e16c906c9a4d97
Nixkey <pokeloquendo102@gmail.com>
2024-03-19 15:02:48 -0300

    nuevo

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -9,6 +9,7 @@ import sys
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
+        exit()
 
     elif num == 0: 
         return 1
@@ -23,10 +24,14 @@ def factorial(num):
 if len(sys.argv) == 0:
    print("Debe informar un número!")
    sys.exit()
+
+#inf = int(input("Número inferior hasta 1: "))
+inf_dos = int(input("Número inferior hasta 60: "))
+
 num=int(input("Número: "))
 
-if (num < 4 or num > 8):
-    print("Rango es entre 4 y 8")
+if (num < inf_dos or num > 60): #(num > 1 or num < inf) or 
+    print("Rango no permitido")
     exit()
 
 print("Factorial ",num,"! es ", factorial(num)) 
