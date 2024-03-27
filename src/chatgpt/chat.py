111c04af8787aa776466fcfe2af7a07094370cfa
Nixkey <pokeloquendo102@gmail.com>
2024-03-27 16:42:00 -0300

    chatgpt ej2

--- src/chatgpt/chat.py
+++ src/chatgpt/chat.py
@@ -1,24 +1,31 @@
-import openai
+# import openai
 
-consulta = input("Ingrese su consulta:")
+# try:
 
-response = openai.chat.completions.create(
-model="gpt-3.5-turbo-0125",
-messages=[
-{
-"role": "system",
-"content": "context" },
-{
-"role": "user",
-"content" : "usertask" },
-{
-"role": "user",
-"content": consulta }
-],
-temperature=1,
-max_tokens=4096,
-top_p=1,
-frequency_penalty=0,
-presence_penalty=0
-)
-print(response.choices[0].message.content)
+#     consulta = input("Ingrese su consulta:")
+
+#     response = openai.chat.completions.create(
+#     model="gpt-3.5-turbo-0125",
+#     messages=[
+#     {
+#     "role": "system",
+#     "content": "context" },
+#     {
+#     "role": "user",
+#     "content" : "usertask" },
+#     {
+#     "role": "user",
+#     "content": consulta }
+#     ],
+#     temperature=1,
+#     max_tokens=4096,
+#     top_p=1,
+#     frequency_penalty=0,
+#     presence_penalty=0
+#     )
+#     print (f"Su consulta fue {consulta}. Procesando...")
+
+#     print(f"ChatGPT dice: {response.choices[0].message.content}")
+
+# except Exception as ex:
+#     print(f"Error: {ex}")
