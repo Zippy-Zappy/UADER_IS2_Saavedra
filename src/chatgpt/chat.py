2035feefe23afab029f7fa1e39093f418c36de8d
Nixkey <pokeloquendo102@gmail.com>
2024-03-27 16:25:56 -0300

    chatgpt v1

---
+++ src/chatgpt/chat.py
@@ -0,0 +1,24 @@
+import openai
+
+openai.api_key = "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"
+
+response = openai.chat.completions.create(
+model="gpt-3.5-turbo-0125",
+messages=[
+{
+"role": "system",
+"content": "context" },
+{
+"role": "user",
+"content" : "usertask" },
+{
+"role": "user",
+"content": "userquery" }
+],
+temperature=1,
+max_tokens=4096,
+top_p=1,
+frequency_penalty=0,
+presence_penalty=0
+)
+print(response.choices[0].message.content)
