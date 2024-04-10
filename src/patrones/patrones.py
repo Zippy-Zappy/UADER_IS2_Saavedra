cd5fa9c6764fcd2e4f37ced7a398c208d268c77d
Nixkey <pokeloquendo102@gmail.com>
2024-04-10 16:57:05 -0300

    consigna 3

--- src/patrones/patrones.py
+++ src/patrones/patrones.py
@@ -39,6 +39,7 @@ s2 = Singleton()
 print(s1.factorial(1))
 print(s2.factorial(5))
 
+print()
 print("Consigna 2:")
 
 from abc import ABC, abstractmethod
@@ -74,7 +75,7 @@ class FabricaImpuestos(ABC):
     def crearCalculadorContribuciones(self) -> CalculadorContribuciones:
         pass
 
-#Fabricas Concretas
+#Abstract Fabricas Concretas
 class FabricaImpuestosArgentina(FabricaImpuestos):
     def crearCalculadorIVA(self) -> CalculadorImpuestos:
         return CalculadorIVA()
@@ -93,3 +94,77 @@ if __name__ == "__main__":
     calculadora = factory.crearCalculadorIVA()
     total = calculadora.calcular(base_imponible)
     print(f"El total de impuestos sería {total}")
+
+print()
+print("Consigna 3:")
+
+class Factory(ABC):
+    @abstractmethod
+    def metodo_factory(self):
+        pass
+
+    def mostrar_envio(self):
+        #crea el objeto producto
+        producto = self.metodo_factory()
+
+        #se usa el producto
+        resultado = f"Se ha completado la transacción."
+
+        return resultado
+
+#las concrete factory van a sobreescribir el método de operación
+#para cambiar el resultado del producto
+class TipoEnvio(ABC):
+    @abstractmethod
+    def mostrar_envio(self) -> str:
+        pass
+
+class ConcreteFactory1(Factory):
+    def mostrar_envio(self) -> TipoEnvio:
+        return EntregaDelivery()
+    
+class ConcreteFactory2(Factory):
+    def mostrar_envio(self) -> TipoEnvio:
+        return EntregaCliente()
+
+class ConcreteFactory3(Factory):
+    def mostrar_envio(self) -> TipoEnvio:
+        return EntregaMostrador()
+
+
+
+"""
+Productos Concretos, implementan la interfaz de Producto (TipoEnvio)
+"""
+
+
+class EntregaDelivery(TipoEnvio):
+    def mostrar_envio(self) -> str:
+        return "Enviaremos al cadete."
+
+class EntregaCliente(TipoEnvio):
+    def mostrar_envio(self) -> str:
+        return "Lo esperaremos en el local."
+
+class EntregaMostrador(TipoEnvio):
+    def mostrar_envio(self) -> str:
+        return "Retire su producto por el mostrador."
+
+def client_code(creator: Factory) -> None:
+    """
+    The client code works with an instance of a concrete creator, albeit through
+    its base interface. As long as creator.some_operation() the client keeps working with the creator via
+    the base interface, you can pass it any creator's subclass.
+    """
+    #Yo soy un mero intermediario tipo FACTORY que no se que es lo que estoy creando, pero lo creo.
+    print(f"Operación completa. {creator.mostrar_envio()}", end="")
+
+if __name__ == "__main__":
+    print("Entrega por delivery:")
+    client_code(EntregaDelivery())
+    print()
+    print("\nEntrega por mostrador:")
+    client_code(EntregaMostrador())
+    print()
+    print("\nUsted se dirigirá al local.")
+    client_code(EntregaCliente())
