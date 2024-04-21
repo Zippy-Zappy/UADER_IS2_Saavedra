edf44369087b2819d34a45e3b3f015581b091d40
Nixkey <pokeloquendo102@gmail.com>
2024-04-21 17:57:32 -0300

    TP 5 Consigna 3

--- src/patrones/patrones_comportamiento.py
+++ src/patrones/patrones_comportamiento.py
@@ -163,3 +163,126 @@ if __name__ == "__main__":
     print("Recorrido reverso:")
     print("\n".join(coleccion.get_iterador_reverso()), end="")
     print("\n")
+
+print()
+print("Consigna 3:")
+'''
+Implemente una clase bajo el patrón observer donde una serie de clases están
+subscriptas, cada clase espera que su propio ID (una secuencia arbitraria de 4
+caracteres) sea expuesta y emitirá un mensaje cuando el ID emitido y el propio
+coinciden. Implemente 4 clases de tal manera que cada una tenga un ID
+especifico. Emita 8 ID asegurándose que al menos cuatro de ellos coincidan con
+ID para el que tenga una clase implementada.
+'''
+import random
+
+class Sujeto(ABC):
+    @abstractmethod
+    def attach(self, observador: Observador) -> None:
+        pass
+
+    @abstractmethod
+    def detach(self, observador: Observador) -> None:
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        pass
+    
+class Observador(ABC):
+    @abstractmethod
+    def update(self, sujeto: Sujeto) -> None:
+        pass
+
+class ConcreteSubject(Sujeto):
+    """
+    The Subject owns some important state and notifies observers when the state
+    changes.
+    """
+
+    _state: str = None
+    """
+    For the sake of simplicity, the Subject's state, essential to all
+    subscribers, is stored in this variable.
+    """
+
+    _observers: List[Observador] = []
+    """
+    List of subscribers. In real life, the list of subscribers can be stored
+    more comprehensively (categorized by event type, etc.).
+    """
+
+    def attach(self, observador: Observador) -> None:
+        print("Sujeto: agregué un observador.")
+        self._observers.append(observador)
+
+    def detach(self, observador: Observador) -> None:
+        self._observers.remove(observador)
+
+    """
+    The subscription management methods.
+    """
+
+    def notify(self) -> None:
+        """
+        Trigger an update in each subscriber.
+        """
+
+        print("Sujeto: notificando a los observadores...")
+        for observer in self._observers:
+            observer.update(self)
+
+    def some_business_logic(self) -> None:
+        """
+        Usually, the subscription logic is only a fraction of what a Subject can
+        really do. Subjects commonly hold some important business logic, that
+        triggers a notification method whenever something important is about to
+        happen (or after it).
+        """
+
+        print("\nSujeto: estoy haciendo algo...")
+        lista = ["ABCD", "EFGH", "WXYZ", "LALO", "IJKL", "DCBA", "MNOPQ", "LEET"]
+        self._state = random.choice(lista)
+
+        print(f"Sujeto: mi estado cambió a: {self._state}")
+        self.notify()
+
+class ObservadorA(Observador):
+    def update(self, sujeto: Sujeto) -> None:
+        if sujeto._state == "WXYZ":
+            print("Observador XWYZ reacciona al evento.")
+
+class ObservadorB(Observador):
+    def update(self, sujeto: Sujeto) -> None:
+        if sujeto._state == "ABCD":
+            print("Observador ABCD reacciona al evento.")
+
+class ObservadorC(Observador):
+    def update(self, sujeto: Sujeto) -> None:
+        if sujeto._state == "LALO":
+            print("Observador LALO reacciona al evento.")
+
+class ObservadorD(Observador):
+    def update(self, sujeto: Sujeto) -> None:
+        if sujeto._state == "EFGH":
+            print("Observador EFGH reacciona al evento.")
+        
+if __name__ == "__main__":
+    # The client code.
+
+    subject = ConcreteSubject()
+
+    observer_a = ObservadorA()
+    subject.attach(observer_a)
+
+    observer_b = ObservadorB()
+    subject.attach(observer_b)
+
+    observer_c = ObservadorC()
+    subject.attach(observer_c)
+
+    observer_d = ObservadorD()
+    subject.attach(observer_d)
+
+    subject.some_business_logic()
+    subject.some_business_logic()
