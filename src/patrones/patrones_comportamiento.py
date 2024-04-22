7a17734fd2687125ad2dfc61927d2a34aa7de333
Nixkey <pokeloquendo102@gmail.com>
2024-04-22 17:05:29 -0300

    TP 5 Consigna 5

--- src/patrones/patrones_comportamiento.py
+++ src/patrones/patrones_comportamiento.py
@@ -375,3 +375,84 @@ if __name__ == "__main__":
 
 print()
 print("Consigna 5:")
+'''
+Modifique el programa IS2_taller_memory.py para que la clase tenga la
+capacidad de almacenar hasta 4 estados en el pasado y pueda recuperar los
+mismos en cualquier orden de ser necesario. El método undo deberá tener un
+argumento adicional indicando si se desea recuperar el inmediato anterior (0) y
+los anteriores a el (1,2,3).
+'''
+#*--------------------------------------------------------------------
+#* Design pattern memento, ejemplo
+#*-------------------------------------------------------------------
+class Memento:
+	def __init__(self, file, content):	
+		self.file = file
+		self.content = content
+
+class FileWriterUtility:
+    def __init__(self, file):
+        self.file = file
+        self.content = ""
+        self.estados = []
+
+    def write(self, string):
+        self.content += string
+
+    def undo(self, memento, estado_anterior = 1):
+        if (estado_anterior <= (len(self.estados))):
+            memento = self.estados[-estado_anterior]
+            self.file = memento.file
+            self.content = memento.content
+        else:
+             print("Estado inválido.")
+
+    def save(self):
+        self.estados.append(Memento(self.file, self.content))
+        if len(self.estados) > 4:
+             self.estados.pop(0)
+        #return Memento(self.file, self.content)
+
+class FileWriterCaretaker:
+
+
+	def save(self, writer):
+		self.obj = writer.save()
+
+	def undo(self, writer, estado = 0):
+		writer.undo(self.obj)
+
+
+if __name__ == '__main__':
+	#print("Crea un objeto que gestionará la versión anterior")
+	caretaker = FileWriterCaretaker()
+
+	#print("Crea el objeto cuyo estado se quiere preservar");
+	writer = FileWriterUtility("GFG.txt")
+
+	print("Se graba algo en el objeto y se salva")
+	writer.write("Clase de IS2 en UADER\n")
+	print(writer.content + "\n\n")
+	caretaker.save(writer)
+
+	print("Se graba información adicional")
+	writer.write("Material adicional de la clase de patrones\n")
+	print(writer.content + "\n\n")
+	caretaker.save(writer)
+
+	print("Se graba información adicional II")
+	writer.write("Material adicional de la clase de patrones II\n")
+	print(writer.content + "\n\n")
+
+	print("se invoca al <undo>")
+	caretaker.undo(writer, )
+
+	print("Se muestra el estado actual")
+	print(writer.content + "\n\n")
+
+	print("se invoca al <undo>")
+	caretaker.undo(writer)
+
+	print("Se muestra el estado actual")
+	print(writer.content + "\n\n")
+
